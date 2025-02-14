465f2bb2d7c277b06676036df4e3d8fba162a74f
Жанетта <132734780+zhanimk@users.noreply.github.com>
2025-02-14 09:44:57 +0500

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,32 @@
+.footer {
+    position: absolute;
+    bottom: 0;
+    background-color: #4c0e78;
+    color: white;
+    text-align: right;
+    padding: 10px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+}
+.footer p {
+    margin: 0;
+    font-size: 15px;
+}
+.whatsapp-link,
+.instagram-link,
+.cart-button {
+    background-color: #8A2BE2;
+    color: white;
+    padding: 12px 30px;
+    border-radius: 30px;
+    font-size: 16px;
+    text-decoration: none;
+    display: inline-block;
+    margin-left: 20px;
+}
+.whatsapp-link:hover,
+.instagram-link:hover,
+.cart-button:hover {
+    background-color: #4c0e78;
+}
